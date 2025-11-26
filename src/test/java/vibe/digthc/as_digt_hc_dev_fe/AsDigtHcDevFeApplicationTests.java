7c010063e224596426c41027f83233faa0acf5a6
ByDeok <ejrwls36400@gmail.com>
2025-11-26 21:34:18 +0900

    add java

---
+++ src/test/java/vibe/digthc/as_digt_hc_dev_fe/AsDigtHcDevFeApplicationTests.java
@@ -0,0 +1,13 @@
+package vibe.digthc.as_digt_hc_dev_fe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsDigtHcDevFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
