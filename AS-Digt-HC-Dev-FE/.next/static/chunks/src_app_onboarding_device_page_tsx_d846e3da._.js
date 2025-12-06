8f10a687529ba3013fe3eb49f728de72c2109b84
ByDeok <ejrwls36400@gmail.com>
2025-12-06 17:58:16 +0900

    TEST FAIL2

--- AS-Digt-HC-Dev-FE/.next/static/chunks/src_app_onboarding_device_page_tsx_d846e3da._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_4b2f2bf9._.js"
-],
-    source: "dynamic"
-});
